3554c544558f3f1b76e5cef0c05b3b7099f6d5a1
Tanner <tannersmithasdf@hotmail.com>
2019-05-06 18:45:56 -0500

    Remove nested try catch and remove separate discriminator field
    Moved the discriminator to the author instead of having its own field.

--- commands/utility/userinfo.js
+++ commands/utility/userinfo.js
@@ -8,26 +8,25 @@ exports.run = async (handler, message, args, pre) => {
 
 		try {
 			user = await message.client.fetchUser(args.user)
-			try {
-				member = await message.guild.fetchMember(user)
-			} catch (exception) {
-				return message.channel.send('User is not in the server.')
-			}
+			member = await message.guild.fetchMember(user)
 		} catch (exception) {
-			return message.channel.send('Invalid user.')
+			if (user) {
+				return message.channel.send('User is not in server.')
+			} else {
+				return message.channel.send('Invalid user.')
+			}
 		}
 	}
 
 	const embed = new Discord.RichEmbed()
 		.setColor(member.displayColor || null)
-		.setAuthor(member.user.username, member.user.avatarURL)
+		.setAuthor(`${member.user.username}#${member.user.discriminator}`, member.user.avatarURL)
 
 	if (member.nickname) {
 		embed.addField('Nickname', member.nickname, true)
 	}
 
-	embed.addField('Discriminator', member.user.discriminator, true)
-		.addField('Date Joined', `${member.joinedAt.toISOString().slice(0, -5)}Z`, true)
+	embed.addField('Date Joined', `${member.joinedAt.toISOString().slice(0, -5)}Z`, true)
 		.addField('User Creation Date', `${member.user.createdAt.toISOString().slice(0, -5)}Z`, true)
 
 	return message.channel.send({ embed })
