7b94950aa84ac5023674dcef2692a20798cdb258
Tanner <tannersmithasdf@hotmail.com>
2019-07-20 10:21:38 -0500

    Add days arg
    Added days arg and updated message sent if no time args given

--- commands/utility/remindme.js
+++ commands/utility/remindme.js
@@ -1,11 +1,11 @@
 exports.run = async (handler, message, args, pre) => {
 	if (!args.hours && !args.minutes && !args.seconds) {
-		return message.channel.send('No arguments given.')
+		return message.channel.send('Days, hours, minutes, or seconds argument must be given.')
 	}
 
-	var time = ((args.hours ? args.hours : 0) * 3600000) + ((args.minutes ? args.minutes : 0) * 60000) + ((args.seconds ? args.seconds : 0) * 1000)
+	var time = ((args.days ? args.days : 0) * 86400000) + ((args.hours ? args.hours : 0) * 3600000) + ((args.minutes ? args.minutes : 0) * 60000) + ((args.seconds ? args.seconds : 0) * 1000)
 
-	message.channel.send(`Set timer for ${args.hours ? `${args.hours} hours, ` : ''}${args.minutes ? `${args.minutes} minutes, ` : ''}${args.seconds ? `${args.seconds} seconds ` : ''}from now${args.reason ? ` for \`${args.reason}\`` : ''}.`)
+	await message.channel.send(`Set timer for ${args.days ? `${args.days} days, ` : ''}${args.hours ? `${args.hours} hours, ` : ''}${args.minutes ? `${args.minutes} minutes, ` : ''}${args.seconds ? `${args.seconds} seconds ` : ''}from now${args.reason ? ` for \`${args.reason}\`` : ''}.`)
 
 	setTimeout(() => {
 		return message.channel.send(`<@${message.member.id}>, Reminding you${args.reason ? ` for \`${args.reason}\`` : ''}!`)
@@ -14,12 +14,13 @@ exports.run = async (handler, message, args, pre) => {
 
 exports.yargsOpts = {
 	alias: {
+        days: ['d'],
 		hours: ['h'],
 		minutes: ['m'],
 		seconds: ['s'],
 		reason: ['r']
 	},
-	integer: ['hours', 'minutes', 'seconds'],
+	integer: ['days', 'hours', 'minutes', 'seconds'],
 	string: ['reason']
 }
 
