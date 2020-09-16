d90fb8e630dab097a79b5f37337dc1da59388dab
FM-96 <felix.muellner.96@gmail.com>
2020-09-16 02:56:46 +0200

    Start using API gateway intents

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ require('./src/utility/configureLogger')
 const Discord = require('discord.js')
 const Handler = require('./src/classes/Handler')
 
-const client = new Discord.Client()
+const client = new Discord.Client({
+	ws: {
+		intents: Discord.Intents.ALL
+	}
+})
 const handler = new Handler(client)
 
 handler.start()
